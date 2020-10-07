6d4875f98c383574277618f2ab82770535fbbde0
Pinefoot <jameslcarpino@gmail.com>
2020-10-07 12:29:25 -0700

    created tables for posting commenting users voting

---
+++ database/migrations/20201007103218_users.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (tbl) => {
+    tbl.increments();
+    tbl.varchar("username").notNullable().unique().index();
+    tbl.varchar("email").notNullable().unique();
+    tbl.varchar("password").notNullable();
+    tbl.boolean("admin").defaultTo(0);
+
+    tbl.timestamp("user_created").defaultTo(knex.fn.now());
+    tbl.vachar("post_id");
+    tbl.varchar("comment_id");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
+};
