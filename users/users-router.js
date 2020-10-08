12a111c18dac86835c5e90d0d541f2a1b0d1bd16
Pinefoot <jameslcarpino@gmail.com>
2020-10-07 20:44:30 -0700

    issues with admin

--- users/users-router.js
+++ users/users-router.js
@@ -8,17 +8,27 @@ router.use(restricted);
 router.get("/", (req, res) => {
   Users.find()
     .then((users) => {
-      res.status(200).json({ users, jwt: req.jwt });
+      users = JSON.parse(users);
+      console.log(data);
+
+      if (users.admin === true) {
+        res.status(201).json({ users });
+      } else {
+        res
+          .status(200)
+          .json({ message: "Please contact an admin to get user list" });
+      }
     })
     .catch((err) => {
       res.status(403).json(err, { message: "you cant enter" });
     });
 });
 
+//get users by id
 router.get("/:id", (req, res) => {
   Users.findUserId(req.params.id)
     .then((users) => {
-      if (users) {
+      if (users.length > 0) {
         res.status(200).json(users);
       } else {
         res
@@ -31,6 +41,7 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//edit user? why would I need this? leaving it, maybe the way to have admins allow editing for changing admin privalages.
 router.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
@@ -49,6 +60,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
+//delete user by id
 router.delete("/:id", (req, res) => {
   Users.deleteUser(req.params.id).then((removed) => {
     if (removed > 0) {
@@ -61,7 +73,33 @@ router.delete("/:id", (req, res) => {
   });
 });
 
-//get users comments
-router.get("/:id/comments", (req, res) => {});
+//get users comments by users id
+router.get("/:id/comments", (req, res) => {
+  Users.getAllUsersComments(req.params.id)
+    .then((user_comments) => {
+      if (user_comments) {
+        res.status(200).json(user_comments);
+      } else {
+        res.status(404).json({ message: "This user has no comments" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+router.get("/:id/posts", (req, res) => {
+  Users.getAllUsersPosts(req.params.id)
+    .then((user_posts) => {
+      if (user_posts) {
+        res.status(200).json(user_posts);
+      } else {
+        res.status(404).json({ message: "This user has not posted yet." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
 
 module.exports = router;
