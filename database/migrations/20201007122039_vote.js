7ff5ae47d39edff1bdb28ab011f0cd79a922c6ff
Pinefoot <jameslcarpino@gmail.com>
2020-10-14 10:41:01 -0700

    vote table back

---
+++ database/migrations/20201007122039_vote.js
@@ -0,0 +1,31 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("vote", (tbl) => {
+    tbl.increments();
+
+    tbl.boolean("up_voted").defaultTo(false);
+    tbl.boolean("down_voted");
+    tbl.integer("amount_up").defaultTo(0);
+    tbl.integer("amount_down").defaultTo(0);
+    tbl
+      .integer("post_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("posts")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+
+    tbl
+      .integer("commment_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("comments")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("vote");
+};
