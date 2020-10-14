7f8356469baf8a875fb39fad68eca6cb9c7697df
Pinefoot <jameslcarpino@gmail.com>
2020-10-14 16:41:15 -0700

    things working for flags, admin controlled

--- functionality/flags/flags-router.js
+++ functionality/flags/flags-router.js
@@ -2,13 +2,17 @@ const router = require("express").Router();
 const Flags = require("../flags/flags-model");
 const restricted = require("../../authorized/restricted-model");
 
+router.use(restricted);
 //return flagged posts
 router.get("/", (req, res) => {
   Flags.checkAllContent(req.query)
     .then((flags) => {
-      console.log(flags);
-      if (flags) {
+      console.log(req.jwt);
+
+      if (req.jwt.admin === true) {
         res.status(200).json({ flagged_content: flags });
+      } else {
+        res.status(400).json({ message: "Admins only nerd." });
       }
     })
     .catch((err) => {
@@ -19,10 +23,38 @@ router.get("/", (req, res) => {
 router.get("/comments", (req, res) => {
   Flags.getAllFlaggedComments(req.query)
     .then((comments) => {
-      if (comments) {
+      if (comments.length > 0 && req.jwt.admin === true) {
         res.status(200).json({ flagged_comments: comments });
       } else {
         res.status(404).json({ message: "No comments have been flagged" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.get("/users", (req, res) => {
+  Flags.getAllFlaggedUsers(req.query)
+    .then((data) => {
+      if (data.length > 0 && req.jwt.admin === true) {
+        res.status(200).json({ flagged_users: data });
+      } else {
+        res.status(404).json({ message: "No users have been flagged" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.get("/posts", (req, res) => {
+  Flags.getAllFlaggedPosts(req.query)
+    .then((data) => {
+      if (data.length > 0 && req.jwt.admin === true) {
+        res.status(200).json({ flagged_posts: data });
+      } else {
+        res.status(404).json({ message: "No posts have been flagged" });
       }
     })
     .catch((err) => {
