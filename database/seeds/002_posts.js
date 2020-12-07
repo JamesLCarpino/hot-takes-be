489dc61827ff31cbac9a4dfd20fa50605345e74d
Pinefoot <jameslcarpino@gmail.com>
2020-12-07 11:39:16 -0800

    updated some seed files for development

--- database/seeds/002_posts.js
+++ database/seeds/002_posts.js
@@ -6,7 +6,6 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex("posts").insert([
         {
-          id: 1,
           title: "New Post user 1",
           content: "Post Content 1",
           user_id: 1,
@@ -14,35 +13,30 @@ exports.seed = function (knex) {
           votes: 500,
         },
         {
-          id: 2,
           title: "New Post user 1 # 2 ",
           content: "Post Content",
           user_id: 1,
           votes: 2,
         },
         {
-          id: 3,
           title: "New Post user 2",
           content: "Post Content",
           user_id: 2,
           votes: 10000,
         },
         {
-          id: 4,
           title: "New Post user 4",
           content: "Post Content",
           user_id: 4,
           votes: 9,
         },
         {
-          id: 5,
           title: "New Post user 4 post 4",
           content: "Post Content",
           user_id: 4,
           votes: 1,
         },
         {
-          id: 6,
           title: "New Post user 4 # 4",
           content: "Post Content",
           user_id: 4,
--- database/seeds/003_comments.js
+++ database/seeds/003_comments.js
@@ -6,32 +6,27 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex("comments").insert([
         {
-          id: 1,
           content: "Comment Content 1 from user 1 on post 1",
           user_id: 1,
           post_id: 1,
           flagged: true,
         },
         {
-          id: 2,
           content: "Comment Content 2 from user 2 on post 1",
           user_id: 4,
           post_id: 1,
         },
         {
-          id: 3,
           content: "Comment Content 3 from user 3 on post 1",
           user_id: 3,
           post_id: 1,
         },
         {
-          id: 4,
           content: "Comment Content 4 from user 2 on post 1",
           user_id: 2,
           post_id: 1,
         },
         {
-          id: 5,
           content: "Comment Content 5 from user 5 on post 2",
           user_id: 5,
 
@@ -39,20 +34,17 @@ exports.seed = function (knex) {
           flagged: true,
         },
         {
-          id: 6,
           content: "Comment Content 6 from user 5 on post 3",
           user_id: 5,
           post_id: 3,
         },
         {
-          id: 7,
           content: "Comment Content 7 from user 5 on post 1",
           user_id: 5,
           post_id: 1,
           flagged: true,
         },
         {
-          id: 8,
           content: "Comment Content 8 from user 4 on post 1",
           user_id: 4,
           post_id: 1,
